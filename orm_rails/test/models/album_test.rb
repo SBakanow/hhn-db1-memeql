e48f0bca2d6ea3c67aa84c7a06a995b385e9ef95
Sergej Bakanow <s.bakanow@gmail.com>
2022-06-20 12:10:43 +0200

    Add ffi gem and some tests

--- orm_rails/test/models/album_test.rb
+++ orm_rails/test/models/album_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class AlbumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "association" do
+    assert_includes artists(:frankhauser).albums, albums(:wait_for_it_ep)
+    assert_includes artists(:ed_sheeran).albums, albums(:divide)
+  end
 end
--- orm_rails/test/models/playlist_test.rb
+++ orm_rails/test/models/playlist_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class PlaylistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "association" do
+    assert_equal playlists(:my_playlist).songs, songs(:planetary, :legendary, :shape_of_you)
+    assert_equal playlists(:best_songs).songs, songs(:legendary, :islandary)
+  end
 end
